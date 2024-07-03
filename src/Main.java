e8e012d3e4f7ab824257158d43447a6501ad1110
ufuomaisaac <ejemudaroufuoma@gmail.com>
2024-07-03 02:58:01 +0100

    try with resources

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        int num = 0;
+
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+
+        } catch (Exception  e){
+            System.out.println("something went wrong ");
+
+        } finally {
+
+        }
+    }
+}
