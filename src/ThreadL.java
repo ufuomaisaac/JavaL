250a827a2651fd24a4bfa3f501829d878c86573b
ufuomaisaac <ejemudaroufuoma@gmail.com>
2024-07-04 01:32:48 +0100

    Thread safety using the keyword synchronize

--- src/ThreadL.java
+++ src/ThreadL.java
@@ -1,32 +1,31 @@
 import java.io.IOException;
 
 // replacing Threads with the Runnable interface with anonymous classes using lamdas
+ class Counter {
+     int count;
+
+     //The keyword snchronized is used here for thread safety
+     public synchronized void increment() {
+         count++;
+     }
+
+}
 public class ThreadL {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        Counter c = new Counter();
 
         Runnable obj1 = () ->
             {
-                for(int i = 1; i < 10; i++){
-                    System.out.println(i);
-                    try{
-                        Thread.sleep(1000);
-                    }catch(InterruptedException e) {
-                        e.printStackTrace();
-
-                    }
+                for(int i = 1; i <= 1000; i++){
+                   c.increment();
                 }
         };
 
         Runnable obj2 = () ->
         {
-            for(int i = 1; i < 10; i++){
-                System.out.println(i);
-                try{
-                    Thread.sleep(1000);
-                }catch(InterruptedException e) {
-                    e.printStackTrace();
-
-                }
+            for(int i = 1; i <= 1000; i++){
+                c.increment();
             }
         };
 
@@ -36,6 +35,14 @@ public class ThreadL {
 
         t1.start();
         t2.start();
+
+        //join function is used to allow t1 to finish its task before continuing with the main Thread
+        t1.join();
+
+        //join function is used to allow t12to finish its task before continuing with the main Thread
+        t2.join();
+
+        System.out.println(c.count);
 
     }
 }
